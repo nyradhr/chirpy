c8d7b8bf85be130834e9f4aa57ef36e0aa1bd6ec
nyradhr <alessio.piccione@hotmail.it>
2025-08-10 22:07:57 +0200

    list chirps by author

--- list_chirps_handler.go
+++ list_chirps_handler.go
@@ -1,15 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/nyradhr/chirpy/internal/database"
+)
 
 func (cfg *apiConfig) listChirpsHandler(w http.ResponseWriter, r *http.Request) {
-	result, err := cfg.db.ListChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Error listing chirps", err)
-		return
+	var dbChirps []database.Chirp
+	var err error
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err := uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID format", err)
+			return
+		}
+		dbChirps, err = cfg.db.ListChirpsByAuthor(r.Context(), authorID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Error listing chirps", err)
+			return
+		}
+	} else {
+		dbChirps, err = cfg.db.ListChirps(r.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Error listing chirps", err)
+			return
+		}
 	}
-	chirps := make([]Chirp, len(result))
-	for i, c := range result {
+	chirps := make([]Chirp, len(dbChirps))
+	for i, c := range dbChirps {
 		chirps[i] = Chirp{
 			ID:        c.ID,
 			CreatedAt: c.CreatedAt,
